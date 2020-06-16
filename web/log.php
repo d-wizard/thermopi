ccbb89cb0659538d0f8737aaf4614cc40352dccb
d-wizard <dw.wizard@gmail.com>
2020-06-15 20:46:52 -0500

    Add ability to set Device Name / Color and HTML/PHP code to show that info.

--- web/log.php
+++ web/log.php
@@ -1,18 +1,30 @@
+<?php
+   // Get the current settings.
+   include 'setGetSettings.php';
+   setGet("");
+?>
+
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="thermo.css">
 <meta name="viewport" content="width=device-width" />
 <title>Log</title>
 </head>
-   <body>
+   <body style='background-color : <?php echo $DeviceColor;?>' >
    
+   <div class="devicebar">
+      <table style="width:100%"><tr>
+         <td><?php echo $DeviceName;?></td>
+         <td><img src="<?php echo $hotColdIconImg17;?>"> <?php echo $currentTemperature;?></td>
+      </tr></table>
+   </div>
    <div class="navbar">
       <a href="status.php">Status</a>
       <a href="basic.php">Settings</a>
       <a href="graph.php">Graph</a>
       <a href="log.php">Log</a>
    </div>
-   <br><br>
+   <br><br><br><br><br>
 
    <center>
       <h1>Log</h1>
