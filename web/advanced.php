2ead87f96e540bc86be1743a5dda2b7fbd1ab3ec
d-wizard <dw.wizard@gmail.com>
2020-09-13 16:40:47 -0500

    When I moved Comfort Range to Advance Page I forgot to have the value actually be updated.

--- web/advanced.php
+++ web/advanced.php
@@ -24,6 +24,7 @@
       // 
       $timeCmds = array(
          'TimeBetweenTempCheck',
+         'SwitchComfortRange',
       );
 
       // 
--- web/basic.php
+++ web/basic.php
@@ -25,7 +25,6 @@
          'TimeOfDayToStart',
          'TimeOfDayToStop',
          'SwitchTemperature',
-         'SwitchComfortRange',
          'SwitchHeatCool'
       );
 
