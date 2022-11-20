038bf8e19d85c63232d533c3d00e0e19013bd458
d-wizard <dw.wizard@gmail.com>
2022-11-19 20:50:31 -0600

    Add python script that will start another python sciprt if it isn't already running.

--- checkStart.py
+++ checkStart.py
@@ -1,29 +1,121 @@
 import sys
+import re
 import os
-import time
+import subprocess
+import argparse
 
-BIN_DIR = os.path.dirname(os.path.realpath(__file__))
+THIS_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+THIS_SCRIPT_FILENAME = os.path.split(os.path.realpath(__file__))[1] 
 
-SCRIPT_NAME = 'mqttLogger.py'
-SCRIPT_PATH    = os.path.join(BIN_DIR, SCRIPT_NAME)
-ERROR_LOG_PATH = os.path.join(BIN_DIR, 'mqttLogger.stderr')
+# Helper Functions.
+class ProcInfo(object):
+   def __init__(self):
+      self.pid = None
+      self.proc = None
+      self.cmds = None
 
-START_CMD = 'python3 ' + SCRIPT_PATH + ' 2>> ' + ERROR_LOG_PATH + ' &'
+def getAllProcesses():
+   retVal = []
+   pidIndex = -1
+   cmdIndex = -1
+   
+   psaxOut = subprocess.Popen(["ps", "ax"], stdout=subprocess.PIPE).communicate()[0]
+   psaxOut = psaxOut.decode('utf-8').strip()
 
-# Add path to the python file needed to run this script (e.g. getAllProcesses)
-sys.path.append(os.path.join(BIN_DIR, '..', 'PythonLibraries'))
-from getAllProcesses import getProcInfo
+   headerLine = psaxOut.split('\n')[0]
+   header = re.findall(r'(\S+)', headerLine)
+   for i in range(len(header)):
+      if header[i] == 'PID':
+         pidIndex = i
+         break
+   
+   for i in range(len(header)):
+      if header[i] == 'COMMAND':
+         cmdIndex = i
+         break
+   
+   if pidIndex == -1 or cmdIndex == -1:
+      return retVal
 
-procInfo = getProcInfo('python3')
+   psaxLines = psaxOut.split('\n')[1:]
+   for line in psaxLines:
+      newProcess = ProcInfo()
+      procInfo = re.findall(r'(\S+)', line)
+      
+      try:
+         newProcess.pid = int(procInfo[pidIndex])
+         newProcess.proc = procInfo[cmdIndex]
+         newProcess.cmds = procInfo[cmdIndex+1:]
+         retVal.append(newProcess)
+      except:
+         pass
 
-running = False
-for p in procInfo:
-   if SCRIPT_NAME in p.cmds:
-      running = True
-   else:
-      for cmd in p.cmds:
-         if SCRIPT_NAME in cmd:
+   return retVal
+
+def getProcInfo(procName):
+   retVal = []
+   allProcesses = getAllProcesses()
+   for proc in allProcesses:
+      match = False
+      if proc.proc == procName:
+         match = True
+      else:
+         try:
+            if proc.proc[-len(procName):] == procName:
+               match = True
+            else:
+               extraCheck = os.sep + procName
+               if proc.proc[-len(extraCheck):] == extraCheck:
+                  match = True
+         except:
+            pass
+            
+      if match:
+         retVal.append(proc)
+
+   return retVal
+
+# Main start
+if __name__== "__main__":
+   # Config argparse
+   parser = argparse.ArgumentParser()
+   pathToScriptCmd = "--pathToScript"
+   parser.add_argument(pathToScriptCmd,  type=str, action="store", dest="scriptPath", help="Path to the script to run.", default=None)
+   parser.add_argument("--argsToScript", type=str, action="store", dest="args", help="Command line arguments.", default=None)
+   args = parser.parse_args()
+
+   if(args.scriptPath == None):
+      print("Need to specify path to the script to run.")
+      exit(0)
+
+   SCRIPT_PATH = os.path.realpath(args.scriptPath)
+   SCRIPT_DIR, SCRIPT_NAME = os.path.split(SCRIPT_PATH)
+   ERROR_LOG_PATH = SCRIPT_PATH + '.stderr'
+
+   ARGS = "" if args.args == None else " " + args.args # Add extra space to separate command from args.
+
+   START_CMD = 'python3 ' + SCRIPT_PATH + ARGS + ' 2>> ' + ERROR_LOG_PATH + ' &'
+
+   procInfo = getProcInfo('python3')
+
+   running = False
+   for p in procInfo:
+      if THIS_SCRIPT_FILENAME in p.cmds and pathToScriptCmd in p.cmds: # Don't include this process in the check.
+         # Found the process running this script right now. Skip past to the next process.
+         pass
+      else:
+         if SCRIPT_NAME in p.cmds:
             running = True
+         else:
+            for cmd in p.cmds:
+               if SCRIPT_NAME in cmd:
+                  running = True
 
-if not running:
-   os.system(START_CMD)
+   if not running:
+      print(START_CMD)
+      os.system(START_CMD)
+      
+
+
+   
+
