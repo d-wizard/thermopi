273a078bd0eabb42548374399ca29abbd5c78018
d-wizard <dw.wizard@gmail.com>
2022-11-19 19:54:17 -0600

    Add MQTT publishing functionality.

--- temperatureSensorMqtt.py
+++ temperatureSensorMqtt.py
@@ -1,11 +1,16 @@
+
+#!/usr/bin/python3
+
 import os
 import time
+import argparse
+import paho.mqtt.publish as publish
 
 PATH_TO_SENSOR = ""
 
 def temp_raw():
    global PATH_TO_SENSOR
-   temp_sensor = PATH_TO_SENSOR #'/sys/bus/w1/devices/28-800000271ffe/w1_slave'
+   temp_sensor = PATH_TO_SENSOR #'/sys/bus/w1/devices/28-xxxxxxxxxxxx/w1_slave'
    f = open(temp_sensor, 'r')
    lines = f.readlines()
    f.close()
@@ -80,5 +85,26 @@ def temperatureSensor_debugTest():
       print(temperatureSensor_getCelsius())
       time.sleep(1)
       
-      
-#temperatureSensor_debugTest()
+# Main start
+if __name__== "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-b", type=str, action="store", dest="broker", help="Broker IP Addr", default="127.0.0.1")
+   parser.add_argument("-p", type=int, action="store", dest="port", help="Broker Port", default=8883)
+   parser.add_argument("-t", type=str, action="store", dest="topic", help="Topic", default=None)
+   parser.add_argument("-u", type=str, action="store", dest="username", help="Auth User Name", default=None)
+   parser.add_argument("-w", type=str, action="store", dest="password", help="Auth Password", default=None)
+   parser.add_argument("-o", type=int, action="store", dest="pollPeriod", help="How often to poll the sensor in seconds", default=30)
+   args = parser.parse_args()
+
+   if args.topic == None:
+      print("Topic needs to be specified")
+      exit(0)
+
+   temperatureSensor_init()
+
+   while True:
+      try:
+         publish.single(args.topic, str(temperatureSensor_getFahrenheit()), hostname=args.broker, port=args.port)
+      except:
+         pass
+      time.sleep(args.pollPeriod if args.pollPeriod > 0 else 30)
