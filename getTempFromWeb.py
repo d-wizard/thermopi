401ef6cd0e057c659335257125535f2765331ad2
d-wizard <dw.wizard@gmail.com>
2022-11-23 19:08:15 -0600

    Make script python3 and MQTT compatable.

--- getTempFromWeb.py
+++ getTempFromWeb.py
@@ -1,31 +1,11 @@
 import os
 import time
-from urllib2 import urlopen
+from urllib.request import urlopen
 from datetime import datetime
+import argparse
+import paho.mqtt.publish as publish
 
 WEATHER_URL = "http://w1.weather.gov/xml/current_obs/display.php?stid=KCID"
-
-
-
-
-################################################################################
-# Generic Helper Functions
-################################################################################
-def appendFile(path, fileText):
-   try:
-      fileId = open(path, 'a')
-      fileId.write(fileText)
-      fileId.close()
-   except:
-      pass
-
-def logMsg(printMsg):
-   longTimeStr  = str(datetime.now())
-   logMsg = '[' + longTimeStr + "] " + printMsg
-   print(logMsg)
-   
-   logPath = os.path.splitext(os.path.realpath(__file__))[0] + '.log'
-   appendFile(logPath, logMsg + '\n')
 
 
 ################################################################################
@@ -35,22 +15,33 @@ def getTempFromWeb(url = WEATHER_URL):
    temperature = None
    try:
       webURL = urlopen(WEATHER_URL)
-      fullXml = webURL.read()
+      fullXml = webURL.read().decode('utf-8')
       currentTempStr = fullXml.split("<temp_f>")[1].split("</temp_f>")[0]
       temperature = float(currentTempStr)
    except:
       pass
    return temperature
 
+# Main start
+if __name__== "__main__":
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-b", type=str, action="store", dest="broker", help="Broker IP Addr", default="127.0.0.1")
+   parser.add_argument("-p", type=int, action="store", dest="port", help="Broker Port", default=8883)
+   parser.add_argument("-t", type=str, action="store", dest="topic", help="Topic", default=None)
+   parser.add_argument("-u", type=str, action="store", dest="username", help="Auth User Name", default=None)
+   parser.add_argument("-w", type=str, action="store", dest="password", help="Auth Password", default=None)
+   parser.add_argument("-o", type=int, action="store", dest="pollPeriod", help="How often to poll the sensor in seconds", default=30)
+   args = parser.parse_args()
 
-################################################################################
-# Test Functions
-################################################################################
-def test():
-   while 1:
-      readTemp = getTempFromWeb(WEATHER_URL)
-      logMsg(str(readTemp))
-      time.sleep(6)
-     
-     
-#test()
+   if args.topic == None:
+      print("Topic needs to be specified")
+      exit(0)
+
+   while True:
+      try:
+         tempF = getTempFromWeb()
+         if tempF != None:
+            publish.single(args.topic, str(tempF), hostname=args.broker, port=args.port)
+      except:
+         pass
+      time.sleep(args.pollPeriod if args.pollPeriod > 0 else 30)
